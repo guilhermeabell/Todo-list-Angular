5e97b5ca65c11492b5f0194e08df9c68d2bff80f
guilhermeabel99 <gpeuvis@gmail.com>
2020-08-10 02:52:52 -0300

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.models';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+public todos: Todo[] = [];
+public title: String = 'Minhas Tarefas';
+public form: FormGroup;
+
+constructor(private fb: FormBuilder) {
+this.form = this.fb.group({
+  title: ['', Validators.compose([
+    Validators.minLength(3),
+    Validators.maxLength(60),
+    Validators.required,
+  ])]
+});
+
+this.load();
+ }
+
+ add() {
+
+const title = this.form.controls['title'].value;
+  const id = this.todos.length + 1;
+  this.todos.push(new Todo(id, title, false));
+  this.save();
+  this.clear();
+ }
+
+ clear() {
+   this.form.reset();
+ }
+
+ remove(todo: Todo){
+   const index = this.todos.indexOf(todo);
+   if(index !== -1) {
+     this.todos.splice(index, 1);
+   }
+   this.save();
+
+
+ }
+
+ markAsDone(todo: Todo) {
+   todo.done = true;
+   this.save();
+
+
+ }
+
+
+ markAsUndone(todo: Todo) {
+   todo.done = false;
+   this.save();
+
+ }
+
+save() {
+  const data = JSON.stringify(this.todos);
+localStorage.setItem('todos', data);
+}
+
+load() {
+  const data = localStorage.getItem('todos');
+  if(data){
+  this.todos = JSON.parse(data);
+}else{
+  this.todos= [];
+}
+}
+
 }
